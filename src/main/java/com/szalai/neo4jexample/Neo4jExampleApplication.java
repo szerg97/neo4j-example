967773335e68fa51c227a58faabe0033d530cbf9
Gergo Szalai <szerg97@gmail.com>
2023-04-24 16:17:46 +0200

    created project

---
+++ src/main/java/com/szalai/neo4jexample/Neo4jExampleApplication.java
@@ -0,0 +1,13 @@
+package com.szalai.neo4jexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Neo4jExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/szalai/neo4jexample/Neo4jExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szalai.neo4jexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
